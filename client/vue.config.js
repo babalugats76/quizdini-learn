72b07b5cb9ccf86eeecd506d2e633715710cffcc
James Colestock <james@colestock.com>
2020-10-28 18:25:42 -0600

    onDrop refinements

--- client/vue.config.js
+++ client/vue.config.js
@@ -11,7 +11,7 @@ module.exports = {
     config
       .plugin('html')
       .tap(args => {
-        args[0].title = "Quizdini Games";
+        args[0].title = "Quizdini Games!";
         return args
       })
   },
