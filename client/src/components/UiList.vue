cc919ef0bb780ad92d9d741ce825440ea1cd55f4
James Colestock <james@colestock.com>
2020-12-21 16:15:51 -0700

    worked on multi-component ui solution plus jsx functional component experimentation

---
+++ client/src/components/UiList.vue
@@ -0,0 +1,110 @@
+<script>
+//import { h } from "vue";
+import classnames from "classnames";
+
+//import AppIcon from "@/components/AppIcon";
+
+export const UiList = {
+  name: "ui-list",
+  props: {
+    size: {
+      type: String,
+      required: false,
+    },
+    tag: {
+      type: String,
+      default: "div",
+    },
+  },
+  render() {
+    const Tag = `${this.tag}`;
+    return (
+      <Tag class={classnames("ui-list", `ui-list--${this.size}`)}>
+        {this.$slots.default()}
+      </Tag>
+    );
+    /*return h(
+      this.tag,
+      {
+        class: classnames("ui-list", `ui-list--${this.size}`),
+      },
+      this.$slots.default()
+    );*/
+  },
+};
+
+export const UiListItem = {
+  name: "ui-list-item",
+  props: {
+    dense: {
+      type: Boolean,
+    },
+    id: {
+      type: [String, Number],
+    },
+    tag: {
+      type: String,
+      default: "div",
+    },
+  },
+  render() {
+    const Tag = `${this.tag}`;
+    return (
+      <Tag
+        class={classnames("ui-list__item", {
+          "ui-list__item--dense": this.dense,
+          [`ui-list__item--${this.id}`]: this.id,
+        })}
+      >
+        {this.$slots.default()}
+      </Tag>
+    );
+
+    /*return h(
+      this.tag,
+      {
+        class: classnames("ui-list__item", {
+          "ui-list__item--dense": this.dense,
+        }),
+      },
+      this.$slots.default()
+    );*/
+  },
+};
+
+export const UiListIcon = {};
+
+export default UiList;
+</script>
+
+<style lang="scss">
+$list-padding: 0 0 0 0.75rem;
+$list-sizes: (
+  "xs": 0.75rem,
+  "sm": 0.875rem,
+  "md": 1rem,
+  "lg": 1.1875rem,
+  "xl": 1.375rem,
+);
+
+.ui-list {
+  display: block;
+  padding: $list-padding;
+  @include fontSize($list-sizes, "md"); // the "default"
+  @include sizes($list-sizes);
+  &__item {
+    $item: &;
+    font-size: 1.25em;
+    line-height: 1.25;
+    font-family: "Montserrat";
+    font-weight: 500;
+    letter-spacing: 0em;
+    &--dense {
+      letter-spacing: $tracking-tight;
+    }
+    + #{$item} {
+      margin-top: 0.5rem;
+    }
+  }
+}
+</style>
--- client/src/views/UI.vue
+++ client/src/views/UI.vue
@@ -7,6 +7,30 @@
       <app-icon class="my-custom-class" name="watch" />
     </div>
     <h1>Lists</h1>
+    <ui-list
+      class="my-new-ui-component"
+      tag="div"
+      size="xl"
+      :style="{ color: 'red' }"
+    >
+      <ui-list-item
+        :style="{ display: 'inline-block' }"
+        id="yo"
+        key="yo"
+        dense
+        class="duration"
+        >bluh</ui-list-item
+      >
+    </ui-list>
+    <my-list
+      class="splash__details"
+      size="xs"
+      :style="{ color: 'red' }"
+      :items="[
+        { id: 'duration', icon: 'watch', label: '90 seconds' },
+        { id: 'items-per-board', icon: 'grid', label: '9 per board' },
+      ]"
+    ></my-list>
     <app-list class="ui__list" size="sm">
       <app-list-item dense key="duration" class="duration">
         <app-list-icon icon="watch" />90 seconds
@@ -26,6 +50,20 @@ import AppIcon from "@/components/AppIcon";
 import AppList from "@/components/AppList";
 import AppListItem from "@/components/AppListItem";
 import AppListIcon from "@/components/AppListIcon";
+import { UiList, UiListItem } from "@/components/UiList.vue";
+
+const MyList = ({ items, size, ...rest }) => (
+  <UiList tag="div" size={size} {...rest}>
+    {items.map((item) => {
+      const { icon = "", id, label, tag = "div", ...rest } = item;
+      return (
+        <UiListItem dense tag={tag} key={id} id={id} icon={icon} {...rest}>
+          {label}
+        </UiListItem>
+      );
+    })}
+  </UiList>
+);
 
 export default {
   name: "UI",
@@ -34,6 +72,9 @@ export default {
     AppList,
     AppListItem,
     AppListIcon,
+    MyList,
+    UiList,
+    UiListItem,
   },
 };
 </script>
