daf15c84652f69ea32d9ee79be157d72fe3423a0
James Colestock <james@colestock.com>
2021-01-25 13:38:42 -0700

    %npm_config_comment%

--- client/sitemapGenerator.js
+++ client/sitemapGenerator.js
@@ -5,13 +5,15 @@ const AXIOS_CONFIG = { timeout: 30000 };
 
 async function generateSitemap() {
   console.log("generating sitemap...");
+  const matches = await getMatches();
+  console.log(matches);
+}
 
-  const matches = await axios.get(
+async function getMatches() {
+  return await axios.get(
     "https://polar-dawn-13501.herokuapp.com/api/matches",
     AXIOS_CONFIG
   );
-
-  console.log(matches);
 }
 
 generateSitemap();
--- client/vue.config.js
+++ client/vue.config.js
@@ -1,5 +1,40 @@
 const path = require("path");
+
+const SitemapPlugin = require("sitemap-webpack-plugin").default;
+
+const sitemapOptions = {
+  filename: "sitemap.xml",
+  lastmod: true,
+  changefreq: "hourly",
+  priority: "0.8",
+};
+
+const sitemapPaths = [
+  {
+    path: "/",
+    lastmod: new Date().toISOString().slice(0, 10),
+    priority: 0.8,
+    changefreq: "hourly",
+  },
+];
+
 module.exports = {
+  configureWebpack: (config) => {
+    if (process.env.NODE_ENV === "production") {
+      plugins: [
+        new SitemapPlugin({
+          base: process.env.VUE_APP_LEARN_BASE_URL,
+          paths: sitemapPaths,
+          options: {
+            filename: "sitemap.xml",
+            lastmod: true,
+            changefreq: "monthly",
+            priority: 0.4,
+          },
+        }),
+      ];
+    }
+  },
   css: {
     loaderOptions: {
       scss: {
