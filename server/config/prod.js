92e2127aa689dbb2af9c24ab132f4f8c1653385a
James Colestock <james@colestock.com>
2021-01-26 10:42:18 -0700

    sitemap

--- server/config/prod.js
+++ server/config/prod.js
@@ -1,5 +1,6 @@
 // prod.js - production keys
 module.exports = {
+  learnBaseURL: "https://polar-dawn-13501.herokuapp.com",
   memcacheExpire: process.env.MEMCACHE_EXPIRE,
   memcachierPassword: process.env.MEMCACHIER_PASSWORD,
   memcachierServers: process.env.MEMCACHIER_SERVERS,
--- server/routes/sitemapRoutes.js
+++ server/routes/sitemapRoutes.js
@@ -1,16 +1,44 @@
 const mongoose = require("mongoose");
 const xml2js = require("xml2js");
+const keys = require("../config/keys");
 const Match = mongoose.model("matches");
+const { format } = require("date-fns-tz");
 
 module.exports = (app, memcache) => {
   app.get("/sitemap.xml", async (req, res, next) => {
     try {
       res.setHeader("Content-Type", "application/xml");
+
+      const matches = [];
+
+      const cursor = Match.aggregate([
+        {
+          $project: {
+            _id: 0,
+            matchId: 1,
+            lastMod: { $max: ["$createDate", "$updateDate"] },
+          },
+        },
+      ]).cursor();
+
+      // Perform "entity escaping"
+      for await (const doc of cursor) {
+        matches.push({
+          loc: (keys.learnBaseURL + "/" + doc.matchId)
+            .replace(/&/g, "&amp;")
+            .replace(/"/g, "&quot;")
+            .replace(/'/g, "&apos;")
+            .replace(/>/g, "&gt;"),
+          lastmod: format(doc.lastMod, "yyyy-MM-dd'T'HH:mm:ssXXX"),
+        });
+      }
+
       var sitemap = {
         urlset: {
           $: {
             xlmns: "http://www.sitemaps.org/schemas/sitemap/0.9",
           },
+          url: matches,
         },
       };
 
@@ -20,6 +48,7 @@ module.exports = (app, memcache) => {
           pretty: true,
         },
       });
+
       var xml = builder.buildObject(sitemap);
       res.send(xml);
     } catch (e) {
