82af821ea5666f9718f15ff6b23d652a1b44cdfe
James Colestock <james@colestock.com>
2021-01-26 11:21:27 -0700

    commented sitemap code; add caching code

--- server/routes/sitemapRoutes.js
+++ server/routes/sitemapRoutes.js
@@ -2,14 +2,25 @@ const mongoose = require("mongoose");
 const xml2js = require("xml2js");
 const keys = require("../config/keys");
 const Match = mongoose.model("matches");
-const { format } = require("date-fns-tz");
+const { format } = require("date-fns");
 
 module.exports = (app, memcache) => {
   app.get("/sitemap.xml", async (req, res, next) => {
     try {
       res.setHeader("Content-Type", "application/xml");
 
+      // Lookup sitemap in cache; if found, return...
+      const sitemapKey = "sitemap";
+      const cached = await memcache.get(sitemapKey);
+      if (cached) return res.send(cached);
+
       const matches = [];
+
+      /***
+       * Using cursor because free atlas tier
+       * does not allow for use of $function
+       * to add custom JavaScript functions in the pipeline
+       */
 
       const cursor = Match.aggregate([
         {
@@ -21,13 +32,18 @@ module.exports = (app, memcache) => {
         },
       ]).cursor();
 
-      // Perform "entity escaping"
+      /***
+       * Perform "entity escaping"
+       * Format date ISO 8601
+       * See: https://www.w3.org/TR/NOTE-datetime
+       */
       for await (const doc of cursor) {
         matches.push({
           loc: (keys.learnBaseURL + "/match/" + doc.matchId)
             .replace(/&/g, "&amp;")
             .replace(/"/g, "&quot;")
             .replace(/'/g, "&apos;")
+            .replace(/</g, "&lt;")
             .replace(/>/g, "&gt;"),
           lastmod: format(doc.lastMod, "yyyy-MM-dd'T'HH:mm:ssXXX"),
         });
@@ -50,6 +66,10 @@ module.exports = (app, memcache) => {
       });
 
       var xml = builder.buildObject(sitemap);
+
+      // Write sitemap data to cache
+      memcache.set(sitemapKey, xml);
+
       res.send(xml);
     } catch (e) {
       next(e);
